eb2d4a99180f91cab8e48cd6d514ffdc2a531af8
Zolotou <vadimkarulin@gmail.com>
2021-03-30 20:54:08 +0300

    feat: initialize redux Co-authored-by: Maslovars <Maslovars@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import './App.scss';
 import React, {useState, useEffect} from 'react';
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
+import Main from "./pages/Main/Main";
 
 function App() {
   const [state, setState] = useState("Name of the game");
   const takeName = async () => {
-    const api_host = "https://cors-anywhere.herokuapp.com/"
     const url = 'http://steamspy.com/api.php?request=top100in2weeks';
-    const res = await fetch(api_host + url);
+    const res = await fetch(url);
     const data = await res.json();
-    const array = []
-    for (let game in data){
-      array.push(game);
-    }
-    setState(array.map(game => state.push(game.name)));
-    console.log(state);
+    const array = Object.entries(data).map((item) => ({...item[1]})).sort((first, second) => second.ccu - first.ccu)
+    console.log(array)
+    // const array = []
+    // for (let game in data){
+    //   array.push(game);
+    // }
+    setState("lol");
+    // console.log(data);
   }
+
   return (
     <div className="App">
         <Header />
@@ -26,5 +28,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -11,7 +11,7 @@ const Main = ({takeName, nameOfTheGame}) => {
             {nameOfTheGame}
           </section>
         
-        <p>Start the project</p>
+        <p>Start  project</p>
         </main>
     )
 }
