5e1959056e6ff032b53981915249360cc212fad9
Maslovars <maslovars@gmail.com>
2021-04-20 06:08:33 +0300

    add links and icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,29 @@
+import { Link, NavLink } from "react-router-dom";
+import styles from "./Footer.module.scss";
+import logo from "../../assets/Steam_icon_logo.svg";
+import github from "../../assets/github-icon-1.svg";
+import linkedin from "../../assets/linkedin-icon-1.svg";
+
+
 const Footer = () => {
     return (
-        <footer>Footer</footer>
+        <footer className={styles.footer}>
+            <div className={styles.footer__body}>
+                <NavLink exact to="/"><img src={logo} alt="logo" /></NavLink>
+                <div className={styles.nav}>
+                    <NavLink activeClassName={styles.active} exact to="/">Main</NavLink>
+                    <NavLink activeClassName={styles.active} to="/score" >Score</NavLink>
+                    <NavLink activeClassName={styles.active} to="/about">About</NavLink>
+                </div>
+                <div>
+                    <div>Arseny Maslov <Link to="https://github.com/Maslovars"><img className={styles.icon} src={github} alt="github" /></Link> |
+                    <Link to="https://www.linkedin.com/in/arseniy-maslov/"><img className={styles.icon} src={linkedin} alt="linkedin" /></Link> </div>
+                    <div>Vadim Karulin <Link to="https://github.com/Zolotou"><img className={styles.icon} src={github} alt="github" /></Link> |
+                    <Link to="https://www.linkedin.com/in/vadim-karulin/"><img className={styles.icon} src={linkedin} alt="linkedin" /></Link> </div>
+                </div>
+            </div>
+            <div className={styles.copyright}>© 2021 Minsk Belarus Wild Code School</div>
+        </footer>
     )
 }
 
