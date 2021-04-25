1a332f32c54a572bf95f9463fa96ec6747c3f23c
Zolotou <vadimkarulin@gmail.com>
2021-04-25 19:02:46 +0300

    feat: add sounds and fix price

--- src/components/Main/Wallet/Wallet.js
+++ src/components/Main/Wallet/Wallet.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import "./Wallet.scss";
-import gif from '../../../assets/icons8-us-dollar-circled.gif'
+import audioFile from "../../../assets/pence.mp3"
+import gif from '../../../assets/icons8-us-dollar.svg'
+import ReactTooltip from "react-tooltip";
 
-const Wallet = ({cash, onMoney}) => {
-
+const Wallet = ({ cash, onMoney }) => {
+    const [audio] = useState(new Audio(audioFile));
     const [money, setMoney] = useState(0);
 
+    const addCash = () => {
+        audio.volume = 0.5;
+        audio.currentTime = 0;
+        audio.play()
+        onMoney(1);
+    }
+
     return (
-        <div>
-            <img className="dollar" onClick={() => onMoney(1)} src={gif} alt="dollar" />
-        </div>
+        <>
+            <div>
+                <img data-tip data-for="wallet" className="dollar" onClick={() => addCash()} src={gif} alt="dollar" />
+            </div>
+            <ReactTooltip id="wallet" place="top" type="dark" effect="float">
+                <h4>Clicker</h4>
+                <p>Just press button to add cash into your wallet</p>
+            </ReactTooltip>
+        </>
+
     )
 }
 
