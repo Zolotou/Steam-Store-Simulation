3597fa6ded39dd653f56b95c35a8676803f052ab
Zolotou <vadimkarulin@gmail.com>
2021-04-26 21:11:53 +0300

    fix: icons

--- src/components/Main/Stats/Stats.js
+++ src/components/Main/Stats/Stats.js
@@ -1,5 +1,7 @@
 import styles from "./Stats.module.scss";
 import {useState} from 'react'
+import showInventory from "../../../assets/icons8-show-property.svg";
+import hideInventory from "../../../assets/icons8-hide.svg"
 
 function Stats({ cash, boughtGames, inventoryHandle }) {
 
@@ -15,7 +17,16 @@ function Stats({ cash, boughtGames, inventoryHandle }) {
     <div className={styles.stats}>
       <h2>Wallet: {cash} $</h2>
       <h3>Games purchased: {boughtGames}</h3>
-      <button onClick={() => handleClick()}>{state ? "Show inventory" : "hide Inventory"}</button>
+      <div onClick={() => handleClick()}>{state ? 
+        <div>
+          <img src={showInventory} alt="" srcset=""/>
+          <p>show inventory</p>
+        </div> : 
+        <div>
+        <img src={hideInventory} alt="" srcset=""/>
+        <p>hide inventory</p>
+      </div>}
+      </div>
     </div>
   )
 }
